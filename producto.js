7c3bb54fb9b4ae35689a0490d901f8cb1eea59de
Pilar Londoño <104735328+Mapilowi87@users.noreply.github.com>
2022-10-30 23:11:54 -0300

    tercera entrega

--- producto.js
+++ producto.js
@@ -1,59 +1,181 @@
-class Producto{
-    constructor(nombre,talle,precio,cantidad){
-        this.nombre = nombre;
-        this.talle= talle;
-        this.precio = parseFloat (precio);
-        this.cantidad = cantidad;
-    }
-    
-    //metodo del objeto para sumar el iva
-    sumaIva(){
-        return this.precio * 1.21;
-    }
+/***** TU TIENDA ONLINE.COM - CARRITO DE COMPRAS ******/
 
-}
+//¿Qué funcionalidad debe tener un carrito de compras? 
 
-    //instanciamos (creamos)objetos
+//1) Mostrar productos en el HTML de forma dinámica. 
+//2) Agregar productos al carrito. 
+//3) Evitar la carga de productos repetidos en el carrito. 
+//4) Mostrar el carrito en el HTML de forma dinámica. 
+//5) Eliminar productos del carrito. 
+//6) Calcular el total de la compra. 
+//7) Vaciar el carrito. 
+//8) Guardar el carrito en el LocalStorage. 
 
-let arrayProductos = [];
+/////////////////////////////////////////////
 
-const producto0 = new Producto ("bersuit","s",500,200);
-const producto1 = new Producto ("amelie","m",300,50);
-const producto2 = new Producto ("oxford","l",400,20);
-const producto3 = new Producto ("orange","xs",1000,80);
-
-arrayProductos.push(producto0);
-arrayProductos.push(producto1);
-arrayProductos.push(producto2);
-arrayProductos.push(producto3);
-
-console.log(arrayProductos);
-
-// creo un (array vacio)que contiene los objetos 
-const carrito =[];
-
-// creo una funcion
-function mostrarSeleccion(){
-    const productoUsuario = prompt(` Que producto desea comprar: ¿bersuit amelie oxford u orange?`).toLowerCase();
-    const productoElegido = arrayProductos.find(producto => producto.nombre === productoUsuario);
-
-//Creamos nuestro carrito y medoto push
-    carrito.push(productoElegido);
-
-    const continuarComprando = confirm("¿Quiere comprar otra cosa?");
-    if(continuarComprando){
-        mostrarSeleccion();
-    }else{
-        calcularPrecioTotal();
+class Producto {
+    constructor(id, nombre, precio, img) {
+        this.id = id;
+        this.nombre = nombre; 
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = 1; 
     }
 }
-mostrarSeleccion();
+
+const Abrigo = new Producto(1, "Abrigo", 20000, "https://i.ibb.co/BqzQGCX/abrigo.png");
+const bersuit = new Producto(2, "Bersuit", 5000, "https://i.ibb.co/T1QW9BT/bersuit.png");
+const estacion = new Producto(3, "Estacion", 8000, "https://i.ibb.co/Yp1DCW7/estacion.png");
+const Estampado = new Producto(4, "Estampado", 6000, "https://i.ibb.co/6HdxcZt/Estampado.png");
+const oxford = new Producto(5, "Oxford", 3000, "https://i.ibb.co/CKbkzLJ/oxford.png");
+const rayita = new Producto(6, "Rayita", 4000, "https://i.ibb.co/znMZf7X/rayita.png");
+const rojo = new Producto(7,"Rojo", 3000, "https://i.ibb.co/YWcRxWJ/rojo.png");
+const tejido = new Producto(8, "Tejido", 10000, "https://i.ibb.co/TWdG0py/tejido3.png");
+
+//Creamos un Array con todo nuestro catálogo de productos: 
+
+const productos = [Abrigo, bersuit, estacion, Estampado, oxford, rayita, rojo, tejido];
+
+//Creamos el array carrito 
+
+let carrito = [];
+
+/*** CARGAR CARRITO DESDE EL LOCALSTORAGE: ***/
+//Si hay algo en el localStorage, lo cargamos en el carrito. 
+if(localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+}
+
+console.log(productos);
+
+//Modificamos el DOM mostrando los productos: 
+
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+// Creamos una función para mostrar los productos: 
+
+const mostrarProductos = () => {
+    productos.forEach((producto) => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class="card-body">
+                <h5 class="card-title"> ${producto.nombre} </h5>
+                <p class="card-text"> ${producto.precio} </p>
+                <button class="btn colorBoton" id="boton${producto.id}"> Agregar al Carrito </button>
+                </div>
+            </div>
+        `
+        contenedorProductos.appendChild(card);
+
+        //Agregar productos al carrito: 
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener("click", () => {
+            agregarAlCarrito(producto.id)
+        })
+    })
+}
+
+//Función agregar al carrito: 
+
+const agregarAlCarrito = (id) => {
+    const producto = productos.find((producto) => producto.id === id);
+    const productoEnCarrito = carrito.find((producto) => producto.id === id);
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++;
+    }else {
+        carrito.push(producto);
+        //Al final de la clase, guardamos en el localStorage. 
+        //Trabajamos con el localStorage: 
+        localStorage.setItem("carrito",JSON.stringify(carrito));
+    }
+    calcularTotal();
+}
+
+mostrarProductos();
+
+//MOSTRAR EL CARRITO DE COMPRAS: 
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+});
+
+//Función para mostrar el Carrito: 
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML="";
+    carrito.forEach((producto) => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class="card-body">
+                <h5 class="card-title"> ${producto.nombre} </h5>
+                <p class="card-text"> ${producto.precio} </p>
+                <p class="card-text"> ${producto.cantidad} </p>
+                <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar Producto </button>
+                </div>
+            </div>
+        `
+        contenedorCarrito.appendChild(card);
+
+        //Eliminar productos del carrito: 
+        const boton = document.getElementById(`eliminar${producto.id}`);
+        boton.addEventListener("click", () => {
+            eliminarDelCarrito(producto.id);
+        })
+    })
+    calcularTotal();
+}
 
 
-//creamos una funcion
+//Función que elimina el producto del carrito: 
 
-function calcularPrecioTotal(){
-    const total = carrito.reduce( (acc, elemento ) => acc += elemento.precio, 0 );
-    alert ("El precio total es:" +total);
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find((producto) => producto.id === id);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice, 1);
+    mostrarCarrito();
+
+    //LocalStorage:
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+//Vaciamos todo el carrito de compras: 
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoElCarrito();
+})
+
+//Función para eliminar todo el carrito: 
+
+const eliminarTodoElCarrito = () => {
+    carrito = [];
+    mostrarCarrito();
+
+    //LocalStorage. 
+    localStorage.clear();
+}
+
+//Mostramos mensaje con el total de la compra 
+
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+    let totalCompra = 0; 
+    carrito.forEach((producto) => {
+        totalCompra += producto.precio * producto.cantidad;
+        //+= es igual a poner totalCompra = totalCompra + producto.precio * producto.cantidad;
+    })
+    total.innerHTML = `Total: $${totalCompra}`;
 }
 
